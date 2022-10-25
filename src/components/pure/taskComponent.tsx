9925c12184b4d15ca190661f2ea248bf8a4cad06
gil <fragilgon@gmail.com>
2022-10-26 00:23:02 +0200

    tabla

--- src/components/pure/taskComponent.tsx
+++ src/components/pure/taskComponent.tsx
@@ -1,23 +1,80 @@
 import { useEffect } from "react";
-import { Task } from "../../models/task.class";
-import '../../styles/task.scss'
+import { NIVELES, Task } from "../../models/task.class";
+import "../../styles/task.scss";
 
 interface Props {
   task: Task;
 }
 
 const TaskComponent = ({ task }: Props) => {
-
   useEffect(() => {
-    console.log('Task created')
-  
+    console.log("Task created");
+
     return () => {
-      console.log(`Task ${task.name} is about to be dismounting`)
+      console.log(`Task ${task.name} is about to be dismounting`);
+    };
+  }, [task]);
+
+  /**Return a Badge based in the level of the task */
+  function taskLevelBadge(){
+    switch(task.level){
+      case NIVELES.NORMAL:
+        return (
+          <h6 className='mb-0'>
+            <span className='badge bg-info'>{task.level}</span>
+          </h6>
+        )
+      case NIVELES.BLOQUEANTE:
+        return (
+          <h6 className='mb-0'>
+            <span className='badge bg-warning'>{task.level}</span>
+          </h6>
+        )
+      case NIVELES.URGENTE:
+        return (
+          <h6 className='mb-0'>
+            <span className='badge bg-danger'>{task.level}</span>
+          </h6>
+        )
+      default:
+        break;
     }
-  }, [task])
-  
+  }
+  /**Return the toggle depending on the state of the task [completed, pending] */
+  function taskIconCompleted(){
+    if (task.completed) {
+      return (<i className="bi bi-toggle-on  ms-2" style={{color:'green', fontWeight:'bold'}}></i>)
+    } 
+    else 
+    {
+     return (<i className="bi bi-toggle-off  ms-2" style={{color:'grey', fontWeight:'bold'}}></i>)
+    }
+  }
+
   return (
-    <div className={`card-task ${task.completed? 'completed':'undone'}`}>
+    <tr className="fw-normal">
+      <th >
+        <span className='ms-1'>{task.name}</span>
+      </th>
+      <td className='align-middle'>
+        <span >{task.description}</span>
+      </td>
+      <td>
+        {/**TODO: sustituir por badge */}
+        {taskLevelBadge()}
+      </td>
+      <td className='align-middle'>
+        {/**TODO: sustituir por iconos */}
+        {taskIconCompleted()}
+        <i className="bi-trash ms-2" style={{color:'tomato', fontSize:'large'}}></i>
+      </td>
+    </tr>
+  );
+};
+
+export default TaskComponent;
+
+/**<div className={`card-task ${task.completed? 'completed':'undone'}`}>
       <h2 className='task-name'> {task.name}</h2>
 
       <h3>{task.description}</h3>
@@ -30,8 +87,4 @@ const TaskComponent = ({ task }: Props) => {
         task.completed ? 'Tarea completada': 'Tarea incompleta'
       }
       </h1>
-    </div>
-  );
-};
-
-export default TaskComponent;
+    </div>*/
--- src/container/task_list.tsx
+++ src/container/task_list.tsx
@@ -2,48 +2,79 @@ import { useEffect, useState } from "react";
 import TaskComponent from "../components/pure/taskComponent";
 import { NIVELES, Task } from "../models/task.class";
 
-//Tareas mook 
+//Tareas mook
 const defaultTask = new Task(
   "Leer",
   "Leer el libro el caliz de fuego",
   true,
   NIVELES.BLOQUEANTE
 );
-const anotherTask =  new Task (
+const anotherTask = new Task(
   "Escribir",
   "Escribir una redacción sobre el libro que acabo de leer",
-  false,
+  true,
   NIVELES.URGENTE
-)
+);
+const thirdTask = new Task(
+  "Copiar",
+  "Copiar toda la mierda de bootstrap",
+  false,
+  NIVELES.NORMAL
+);
+
+const bodyCard = {
+  position: "relative" as "relative",
+  height: "500px",
+};
 
 const TaskList = () => {
   //Estado inicial del componente
-  const [tasks, setTasks] = useState([defaultTask, anotherTask]);
+  const [tasks, setTasks] = useState([defaultTask, anotherTask,thirdTask]);
   const [loading, setLoading] = useState(true);
 
   //Control del ciclo de vida del componente
   useEffect(() => {
+    console.log("Task state has been modified");
+    console.log("Cargando los datos");
+    setLoading(false);
 
-    console.log('Task state has been modified')
-    console.log('Cargando los datos')
-    setLoading(false)
-  
     return () => {
-      console.log('TaskList component is about to be dismounting')
-    }
-  }, [tasks])
-  
-  
+      console.log("TaskList component is about to be dismounting");
+    };
+  }, [tasks]);
+
   return (
-    <div className="taskList">
-      <h1>These are all your tasks:</h1>
-
-      {
-        tasks.map((task : Task, index)=>
-          <TaskComponent task={task} key={index}/>
-        )
-      }
-
+    <div className="taskList col-12">
+      <h1>Task App</h1>
+      <div className="card me-4">
+        {/**CARD HEADER */}
+        <div className="card-header p-3 d-flex justify-content-center">
+          <h5>Your TaskList</h5>
+        </div>
+        {/**CARD BODY */}
+        <div
+          className="card-body"
+          data-mdb-perfect-scrollbar="true"
+          style={bodyCard}
+        >
+          <table className="table table-hover table-bordered border-dark">
+            <thead className="table-dark">
+              <tr>
+                <th scope="col" >Title</th>
+                <th scope="col" >Description</th>
+                <th scope="col" >Priority</th>
+                <th scope="col" >Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/**TODO: ITERAR SOBRE UNA LISTA DE TAREAS */}
+              {tasks.map((task: Task, index) => (
+                <TaskComponent task={task} key={index} />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 };
